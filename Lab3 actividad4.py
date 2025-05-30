74b6fdafbe30e86d46d713031e2e82b8345b8f01
Luiseduar1208 <luieduar08@gmail.com>
2025-05-30 14:42:55 -0600

    Add files via upload

---
+++ Lab3 actividad4.py
@@ -0,0 +1,10 @@
+"""Ejercicio 15- Actividad 4 ENTREGABLE
+ALGORITMO:
+1. archivo <-- abrir archivo
+2. texto <-- cargar contenido del archivo
+3. cuantos <-- funcion_contar(cant_num)
+4. print <-- suma_numeros
+5. print <-- el promedio es suma_numeros/cuantos
+6. print <-- 
+
+"""
